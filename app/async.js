7bdf0adbed0038b0ee03490495f06f8ccd84a3e8
Amir <amajedi@gmail.com>
2016-01-28 21:30:21 -0500

    solving async, objects, modules, and flowControl

--- app/async.js
+++ app/async.js
@@ -2,10 +2,24 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    var deferred = $.Deferred();
+    setTimeout(function () {
+        deferred.resolve(value);
+    }, 0);
+    return deferred.promise();
   },
 
   manipulateRemoteData : function(url) {
-
+    var deferred = $.Deferred();
+    $.get(url).then(function (data) {
+        if (data.hasOwnProperty('people')) {
+            deferred.resolve(_.pluck(data.people, 'name').sort());
+        } else {
+            deferred.reject();
+        }
+    }).fail(function () {
+        deferred.reject();
+    });
+    return deferred.promise();
   }
 };
